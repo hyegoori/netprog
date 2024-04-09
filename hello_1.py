1fe37e5bbe2e6538ad6427167bf54ae4b9824a33
Hyewon Kim <117633516+hyegoori@users.noreply.github.com>
2024-04-09 11:20:58 +0900

    Update hello_1.py

--- hello_1.py
+++ hello_1.py
@@ -1,1 +1,14 @@
-print('Hello IoT!')
+# 친구 이름 리스트 생성
+friends = ["태희", "혜교", "지현"]
+
+# 맨 앞에 새로운 친구 추가
+friends.insert(0, "소희")
+
+# 3번째 위치(인덱스 2)에 새로운 친구 추가
+friends.insert(2, "혜리")
+
+# 마지막에 새로운 친구 추가
+friends.append("채")
+
+# 결과 출력
+print(friends)
